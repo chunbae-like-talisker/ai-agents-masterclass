61c3550ad67d407e229761641e46a9b602fea1ea
chunbae-like-talisker <nsy3576@gmail.com>
2025-11-24 20:41:18 +0900

    #5.7 Outro

--- content-pipeline-agent/tools.py
+++ content-pipeline-agent/tools.py
@@ -1,6 +1,7 @@
 import os, re
 from crewai.tools import tool
-from firecrawl import FirecrawlApp, ScrapeOptions
+from firecrawl import Firecrawl
+from firecrawl.v2.types import Document
 
 
 @tool
@@ -13,33 +14,32 @@ def web_search_tool(query: str):
     Returns
         A list of search results with the website content in Markdown format.
     """
-    app = FirecrawlApp(api_key=os.getenv("FIRECRAWL_API_KEY"))
+    app = Firecrawl(api_key=os.getenv("FIRECRAWL_API_KEY"))
 
     response = app.search(
         query=query,
         limit=5,
-        scrape_options=ScrapeOptions(formats=["markdown"]),
+        sources=["web"],
+        scrape_options={"formats": ["markdown"]},
     )
-
-    if not response.success:
-        return "Error using tool."
 
     cleaned_chunks = []
 
-    for result in response.data:
-        title = result["title"]
-        url = result["url"]
-        markdown = result["markdown"]
+    for result in response.web:
+        if isinstance(result, Document):
+            title = result.metadata.title
+            url = result.metadata.url
+            markdown = result.markdown
 
-        cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
-        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+            cleaned = re.sub(r"\\+|\n+", "", markdown).strip()
+            cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
 
-        cleaned_result = {
-            "title": title,
-            "url": url,
-            "markdown": cleaned,
-        }
+            cleaned_result = {
+                "title": title,
+                "url": url,
+                "markdown": cleaned,
+            }
 
-        cleaned_chunks.append(cleaned_result)
+            cleaned_chunks.append(cleaned_result)
 
     return cleaned_chunks
